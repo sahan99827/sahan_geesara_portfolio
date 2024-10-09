ee606922cc94ba84b63994eef8699c98a0cdbe59
Sahan Geesara <111554133+sahan99827@users.noreply.github.com>
2024-10-09 22:27:13 +0530

    error fix

--- src/router/index.js
+++ src/router/index.js
@@ -9,54 +9,7 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    // {
-    //   path: '/project',
-    //   name: 'project',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../components/Project.vue')
-    // },
-    // {
-    //   path: '/skills',
-    //   name: 'skills',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../components/Skills.vue')
-    // },
-    // {
-    //   path: '/education',
-    //   name: 'education',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../components/Education.vue')
-    // },
-    // {
-    //   path: '/contact',
-    //   name: 'contact',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../components/Contact.vue')
-    // },
-    // {
-    //   path: '/experience',
-    //   name: 'experience',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../components/WorkExperience.vue')
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../components/About.vue')
-    // },
+
   ]
 })
 
