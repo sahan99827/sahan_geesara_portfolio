b898c49564776ba80293d9a4e8538f09bdd101fc
Sahan Geesara <111554133+sahan99827@users.noreply.github.com>
2024-10-09 23:30:41 +0530

    vite conig eidit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -14,6 +13,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': './src'    }
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })
