a4c0fb29f6db1121b99ffc4dce9534a7bdf459ca
Sahan Geesara <111554133+sahan99827@users.noreply.github.com>
2025-12-22 20:12:00 +0530

    add vujs ctificate to my portfor;io

--- src/components/Certificates.vue
+++ src/components/Certificates.vue
@@ -34,6 +34,9 @@
     <div class="col-md-4 mb-5">
       <img class="image" src="/assets/Certificates/10.png" alt="Image Description" width="400px" height="275px">
     </div>
+    <div class="col-md-4 mb-5">
+      <img class="image" src="/assets/Certificates/11.png" alt="Image Description" width="400px" height="275px">
+    </div>
   </div>
   <div class="text-center m-4">
     <button id="prev" class="btn btn-dark m-2">Previous</button>
